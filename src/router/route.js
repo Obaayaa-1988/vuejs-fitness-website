686891ef34c0a7400f45233bfaf6d72461a64b2e
Abigail <ashbella333@gmail.com>
2022-07-20 17:31:15 +0000

    route changed

--- src/router/route.js
+++ src/router/route.js
@@ -1,17 +1,17 @@
-import HomeCurrent from '../components/HomeCurrent.vue'
-import WearsComponent from '../components/WearsComponent.vue'
-import MealComponent from '../components/MealComponent.vue'
+import HomeCurrent from '../views/HomeCurrent.vue'
+import WearsComponent from '../views/WearsComponent.vue'
+import MealComponent from '../views/MealComponent.vue'
 const routes = [
     {
         path: '/',
         name: "home",
         component: HomeCurrent,
-        children: [
-            { path: '', name: "wears", component: WearsComponent, },
-            { path: ':id/Meals', name: "meals", component: MealComponent },
-        ]
+        // children: [
+        //     { path: '', name: "wears", component: WearsComponent },
+        //     { path: '/Meals', name: "meals", component: MealComponent },
+        // ]
     },
-    // { path: '/Wears', name: "wears", component: WearsComponent },
-    // { path: '/Meals', name: "meals", component: MealComponent },
+    { path: '/Wears', name: "wears", component: WearsComponent },
+    { path: '/Meals', name: "meals", component: MealComponent },
 ]
 export default routes;
