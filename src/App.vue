c3fc735085080a17e3765d1c2ea9262edd290a15
Abigail <ashbella333@gmail.com>
2022-07-20 17:31:42 +0000

    header component imported

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div id="app">
+    <header-component />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import HeaderComponent from "./components/HeaderComponent.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    HeaderComponent,
+  },
 };
 </script>
 
 <style>
+@import "./assets/css/main.css";
 </style>
